9a826b7a654bddbe0be3c17411eeeca8c1c55364
João Bertholino <joaobertholino964@gmail.com>
2024-07-28 19:28:54 -0300

    Subindo starter project Spring Boot.

---
+++ src/main/java/dev/joaobertholino/udemyspringwithmongo/UdemySpringWithMongoApplication.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.udemyspringwithmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringWithMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaobertholino/udemyspringwithmongo/UdemySpringWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.udemyspringwithmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringWithMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
